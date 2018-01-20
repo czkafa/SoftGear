72ba2c077ccb9ab54b999d437d740055e43c086d
Marcin <czkafa@gmail.com>
2018-01-20 13:30:20 +0900

    Adding spawning enemies

---
+++ Assets/Scripts/Enemy/EnemyUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoftGear.Enemy
+{
+    public class EnemyUnit : MonoBehaviour
+    {
+        void Init()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SoftGear.Enemy;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform spawnPoint;
+    [SerializeField]
+    EnemyUnit enemyPrefab;
+    // Use this for initialization
+    void Start()
+    {
+        GameObject.Instantiate(enemyPrefab, spawnPoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
